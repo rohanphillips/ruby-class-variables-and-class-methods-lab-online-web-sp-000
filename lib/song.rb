78f9be31b3ab78b76f43db778200027dd125a124
Rohan Phillips <rohan@solutions-insurance.com>
2019-07-31 19:20:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,12 +2,16 @@
 class Song
   attr_accessor :name, :artist, :genre
   @@count = 0
+  @@artists = []
 
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1
+    if @@artists.includes?(artist) == false
+      @@artists << artist
+    end
   end
 
   def count()
